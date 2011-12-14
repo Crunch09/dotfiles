e1d40459d3d086f606a3fe19abdb46e63dcc301a
Florian Thomas <florianthomas90@gmail.com>
2011-12-15 00:36:29 +0100

    Added cake bin temporarily to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH=".:bin:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/home/flo/Code/htdocs/smartcloud/lib/Cake/Console:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
